237516d1e082c97700e327700b65d365cae41834
qton <quintin@smartbombinteractive.com>
2014-03-07 11:03:41 -0700

    initial blog reader commit

---
+++ QSTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  QSTableViewController.h
+//  BlogReader
+//
+//  Created by Quintin Smith on 3/7/14.
+//  Copyright (c) 2014 Quintin Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QSTableViewController : UITableViewController
+@property (nonatomic, strong) NSArray *blogPosts;
+
+@end
