7b46eb374decd97e24a3cbc19ae9e82161d49bcf
qton <quintin@smartbombinteractive.com>
2014-03-07 08:56:14 -0700

    Initial Commit

---
+++ BlogReader/QSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QSAppDelegate.h
+//  BlogReader
+//
+//  Created by Quintin Smith on 3/7/14.
+//  Copyright (c) 2014 Quintin Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlogReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlogReader
+//
+//  Created by Quintin Smith on 3/7/14.
+//  Copyright (c) 2014 Quintin Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "QSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QSAppDelegate class]));
+    }
+}
